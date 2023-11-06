19bd70561cbd2529bcf66b8368838913eaa593b7
Pranay Shah <89774391+pranay-harness@users.noreply.github.com>
2023-11-06 23:41:15 +0530

    Update test1.ts

--- src/main/test1.ts
+++ src/main/test1.ts
@@ -16,7 +16,7 @@ const security = require('../lib/insecurity')
 const challenges = require('../data/datacache').challenges
 const users = require('../data/datacache').users
 
-// vuln-code-snippet start loginAdminChallenge loginBenderChallenge loginJimChallenge
+// vuln-code-snippet start loginAdminChallenge loginBenderChallenge loginJimChallenges
 module.exports = function login () {
   function afterLogin (user: { data: User, bid: number }, res: Response, next: NextFunction) {
     verifyPostLoginChallenges(user) // vuln-code-snippet hide-line
